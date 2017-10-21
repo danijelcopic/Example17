fd3c8d7d37ab5d21540452531dbc4554bc87af79
Danijel Copic <danijelcopic@gmail.com>
2017-10-21 03:23:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Example17
+
+Deo vežbi sa kursa Android Application Development koje drži http://ftninformatika.com/
+
+## Opis
+
+Fragments
+
+## Screenshot
+
+![device-2017-10-21-025835](https://user-images.githubusercontent.com/17581397/31846494-d96c897a-b60c-11e7-9106-e60ec314d52d.png), ![device-2017-10-21-025854](https://user-images.githubusercontent.com/17581397/31846498-dc3d3816-b60c-11e7-97ee-9833c95c58fa.png)
+![device-2017-10-21-030003](https://user-images.githubusercontent.com/17581397/31846501-df870f10-b60c-11e7-9e62-81e0d2228570.png), ![device-2017-10-21-030020](https://user-images.githubusercontent.com/17581397/31846504-e245f6a8-b60c-11e7-9d22-ad763eca3c7b.png), ![device-2017-10-21-030811](https://user-images.githubusercontent.com/17581397/31846520-30b08c54-b60d-11e7-9c9e-dbdb9bb92c18.png)
---
+++ app/src/main/java/com/example/android/example17dc/activities/FirstActivity.java
@@ -0,0 +1,103 @@
+package com.example.android.example17dc.activities;
+
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+
+import com.example.android.example17dc.R;
+import com.example.android.example17dc.fragments.DetailFragment;
+import com.example.android.example17dc.fragments.ListFragment;
+
+public class FirstActivity extends Activity implements ListFragment.OnItemSelectedListener {
+
+    boolean landscape = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+
+        if (savedInstanceState == null) {
+
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ListFragment listFragment = new ListFragment();
+            ft.add(R.id.fl_list, listFragment, "List_Fragment_1");
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+
+
+        if (findViewById(R.id.fl_detail) != null) {
+            landscape = true;
+            getFragmentManager().popBackStack();
+
+            DetailFragment detailFragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.fl_detail);
+            if (detailFragment == null) {
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                detailFragment = new DetailFragment();
+                ft.replace(R.id.fl_detail, detailFragment, "Detail_Fragment_1");
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onItemSelected(int position) {
+
+        if (landscape) {
+
+            DetailFragment detailFragment = (DetailFragment) getFragmentManager().findFragmentById(R.id.fl_detail);
+            detailFragment.updateContent(position);
+        } else {
+
+            DetailFragment detailFragment = new DetailFragment();
+            detailFragment.setContent(position);
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.fl_list, detailFragment, "Detail_Fragment_2");
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+    }
+
+}
+
